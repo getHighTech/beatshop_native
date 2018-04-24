f740144a104d572e30ec619fc52030f5c0bf3d31
= <274173368@qq.com>
2018-04-24 13:22:58 +0800

    集成antd-mobile

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button
+} from 'antd-mobile';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app123!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        < Button >antd - mobile button </Button>
       </View>
     );
   }
